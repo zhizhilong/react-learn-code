bf37ba22d8a3aec9dde7d86e7f264b7e1f861248
linzhilong <1914423858@qq.com>
2022-11-27 23:43:03 +0800

    update

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from './App'
 import store from './redux/store'
 
-React.render(<App />,document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 store.subscribe(() => {
-  React.render(<App />,document.getElementById('root'))
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 })
---
+++ learn-redux/src/redux/store.js
@@ -0,0 +1,11 @@
+/**
+ * 该文件专门用于暴露一个store对象，整个应用只有一个store对象
+ * 
+ * */ 
+
+// 引入createStore，专门用于创建redux中最核心的store对象
+import {createStore} from 'redux';
+// 引入为Count组件服务的reducer
+import countRedux from './count_reduce'
+// 暴露store
+export default  createStore(countRedux)
